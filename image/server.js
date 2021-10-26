beaa3d692f0d1ba33c355751bf415c906140056e
alexcaliman <calimanalexandru495@gmail.com>
2021-10-26 13:09:44 +0000

    Versiunea 2

--- image/server.js
+++ image/server.js
@@ -3,7 +3,9 @@ var http = require('http');
 var handleRequest = function(request, response) {
   console.log('Am primit o interogare pe URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!');
+	response.write("Este versiunea 2.Aplicatia se executa pe podul >>");
+	response.write(host);
+  response.end('| v2 \n');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
